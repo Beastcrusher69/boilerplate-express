d2cf5a7a4d4fa2529365d7a6c6a6ad526dad4a00
Jay <jaykapadia389@gmail.com>
2023-03-27 23:48:20 +0530

    4/12

--- myApp.js
+++ myApp.js
@@ -1,7 +1,47 @@
 let express = require('express');
 let app = express();
 
+ap = __dirname + '/views/index.html';
 
+app.get("/", (req, res) => {
+  return res.sendFile(ap);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
 
 
 
