14950ef350b66e7fcd0821c04733773f1ce9a7b6
Jay <jaykapadia389@gmail.com>
2023-03-28 23:59:59 +0530

    7/12

--- myApp.js
+++ myApp.js
@@ -3,8 +3,27 @@ let app = express();
 
 ap = __dirname + '/views/index.html';
 
+app.use("/public",
+  express.static(__dirname + "/public"));
+
+app.use( (req,res,next) => {
+  console.log("${req.method} ${req.path} - ${req.ip}");
+  next();
+})
+
 app.get("/", (req, res) => {
   return res.sendFile(ap);
+})
+
+app.get("/json",(req,res) => {
+
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+        return res.json({ "message": "HELLO JSON" });
+  }
+  else{
+    return res.json({ "message": "Hello json" });
+  }
+  
 })
 
 
@@ -41,39 +60,5 @@ app.get("/", (req, res) => {
 
 
 
+
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
