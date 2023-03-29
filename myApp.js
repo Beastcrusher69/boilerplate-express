80c9ac26bb7d09521c3d2ffad92c6895a30242d4
Jay <jaykapadia389@gmail.com>
2023-03-29 16:12:42 +0530

    completed basic node and express module

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
 ap = __dirname + '/views/index.html';
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use("/public",
   express.static(__dirname + "/public"));
 
-app.use( (req,res,next) => {
-  console.log("${req.method} ${req.path} - ${req.ip}");
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 })
 
@@ -15,22 +18,35 @@ app.get("/", (req, res) => {
   return res.sendFile(ap);
 })
 
-app.get("/json",(req,res) => {
+app.get("/json", (req, res) => {
 
-  if(process.env.MESSAGE_STYLE === "uppercase"){
-        return res.json({ "message": "HELLO JSON" });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({ "message": "HELLO JSON" });
   }
-  else{
+  else {
     return res.json({ "message": "Hello json" });
   }
-  
+
 })
 
+app.get("/now", (req,res,next) => {
+   req.time = new Date().toString();
+  next();
+},(req,res) => {
+   res.json({"time" : req.time});
+})
 
+app.get("/:word/echo",(req,res) => {
+  return res.json({ echo : req.params.word})
+})
 
+app.get("/name", (req ,res) => {
+  return res.json({ "name": req.query.first + " " + req.query.last})
+})
 
-
-
+app.post("/name",(req,res) => {
+  res.json({name : req.body.first + " " + req.body.last})
+})
 
 
 
